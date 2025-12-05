8bc87be642dcba7d8b61267423bbca5331dd5f02
olgapetrova <oskapetrovaa@yandex.ru>
2025-12-05 15:18:55 +0300

    configure CORS to allow http://localhost:5174 with credentials

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import { loggerMiddleware } from './middlewares';
 import { router } from './routes';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5174",
+    credentials: true,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+}));
 
 app.use(loggerMiddleware)
 app.use(express.json());
